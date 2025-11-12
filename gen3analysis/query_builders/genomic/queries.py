2fbc2b052a7e789d116b533dcbea3c239fca96d6
craigrbarnes <craig.barnes@intelligentlight.com>
2025-11-12 15:37:40 -0600

    remove log write

--- gen3analysis/query_builders/genomic/queries.py
+++ gen3analysis/query_builders/genomic/queries.py
@@ -652,9 +652,6 @@ def query_top_genes(
     gene_s = gene_s.extra(track_scores=True)
     gene_s = gene_s.extra(track_total_hits=True)
 
-    with open("./logs/query_top_genes_query.json", "w") as f:
-        json.dump(s.to_dict(), f, indent=2)
-
     results = gene_s.execute()
 
     hits = results["hits"]["hits"]._l_
